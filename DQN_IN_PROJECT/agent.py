575054292e1234952a96e80c1a6aad1bd0140ec8
Zhao Zihe <2809075376@qq.com>
2019-02-03 19:30:26 +0800

    add LSTM and multisteps
    add LSTM and multisteps

--- DQN_IN_PROJECT/agent.py
+++ DQN_IN_PROJECT/agent.py
@@ -6,11 +6,12 @@ from collections import deque
 from util import TARGET_SCOPE, MAIN_SCOPE
 from model import Network
 
+# from tensorflow.python import debug as tf_debug
 
 class Agent:
-    def __init__(self, session, action_dim, state_dim, batch_size=64,
+    def __init__(self, session, action_dim, state_dim, batch_size=64,  nsteps=1, trace_length=10,hidden_size=10,
                  initial_epsilon=0.5, final_epsilon=0.01, replay_size=50000, gamma=0.9, replace_target_freq=10,
-                 doubleDQN=True, duelingDQN=True, store_ckpt=True, ckpt_interval=100,
+                 doubleDQN=False, duelingDQN=False,DRQN=False, store_ckpt=True, ckpt_interval=100,
                  ckpt_dir='./model'):
         """
         Init agent.
@@ -18,6 +19,9 @@ class Agent:
         :param action_dim: action dimension.
         :param state_dim: state dimension.
         :param batch_size: batch size.
+        :param nsteps: nsteps λ
+        :param trace_length: LSTM trace length.
+        :param hidden_size: Fully connected layer to LSTM layer size.
         :param initial_epsilon: decay of the epsilon for random selection.
         :param final_epsilon: decay of the epsilon for random selection.
         :param replay_size: replay buffer size.
@@ -35,12 +39,17 @@ class Agent:
         self.REPLAY_SIZE = replay_size
         self.DOUBLE_DQN = doubleDQN
         self.DUELING_DQN = duelingDQN
+        self.DRQN = DRQN
         self.GAMMA = gamma
         self.REPLACE_TARGET_FREQ = replace_target_freq
         self.BATCH_SIZE = batch_size
+        self.hidden_size = hidden_size
+        self.trace_length = trace_length
         self.action_dim = action_dim
         self.state_dim = state_dim
 
+        self.nsteps = nsteps
+        self.nstep_buffer = []
         # init experience replay
         self.replay_buffer = deque()
         # init some parameters
@@ -50,16 +59,20 @@ class Agent:
         self.ckpt_dir = ckpt_dir
         self.ckpt_interval = ckpt_interval
 
-        self.main_net = Network(self.state_dim, self.action_dim, self.DUELING_DQN, MAIN_SCOPE)
-        self.target_net = Network(self.state_dim, self.action_dim, self.DUELING_DQN, TARGET_SCOPE)
+        self.main_net = Network(self.state_dim, self.action_dim, self.DUELING_DQN, self.DRQN, hidden_size, MAIN_SCOPE)
+        self.target_net = Network(self.state_dim, self.action_dim, self.DUELING_DQN, self.DRQN, hidden_size, TARGET_SCOPE)
 
         self.global_step = tf.Variable(0, name="global_step", trainable=False)
+        self.state_in = (np.zeros([1, self.hidden_size]), np.zeros([1, self.hidden_size]))
         # saver after defining variables
         self.saver = tf.train.Saver()
         self.create_training_method()
         self.soft_update()
         self.init_checkpoint()
-        session.run(tf.global_variables_initializer())
+        self.session.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+        # self.session = tf_debug.LocalCLIDebugWrapperSession(self.session)
+        writer = tf.summary.FileWriter("./model/", self.session.graph)
+        writer.close()
         pass
 
     def init_checkpoint(self):
@@ -70,16 +83,21 @@ class Agent:
         if not os.path.exists(self.ckpt_dir):
             os.mkdir(self.ckpt_dir)
 
+    def visulize_network_structure(self, writer):
+        summary_writer = tf.summary.FileWriter(writer.log_dir, graph=self.session.graph)
+        summary_writer.close()
+
     def create_training_method(self):
         """
         Define cost and optimizer
         :return:
         """
-        self.action_input = tf.placeholder("float", [None, self.action_dim])  # one hot presentation
-        self.y_input = tf.placeholder("float", [None])
+        self.action_input = tf.placeholder("float", [None, self.action_dim], name="action_input")  # one hot presentation
+        self.y_input = tf.placeholder("float", [None], name="y_input")
         Q_action = tf.reduce_sum(tf.multiply(self.main_net.Q_value, self.action_input), reduction_indices=1)
-        self.cost = tf.reduce_mean(tf.square(self.y_input - Q_action))
-        self.optimizer = tf.train.AdamOptimizer(0.0001).minimize(self.cost)
+        # self.cost = tf.reduce_mean(tf.square(self.y_input - Q_action))
+        self.loss = tf.losses.mean_squared_error(self.y_input, Q_action)
+        self.optimizer = tf.train.RMSPropOptimizer(0.0011).minimize(self.loss)
 
     def soft_update(self):
         """
@@ -109,36 +127,71 @@ class Agent:
         one_hot_action = np.zeros(self.action_dim)
         one_hot_action[action] = 1
 
-        # 存入replay_buffer
-        # self.replay_buffer = deque()
-        self.replay_buffer.append((state, one_hot_action, reward, next_state, done))
+        self.nstep_buffer.append((state, one_hot_action, reward, next_state))
 
-        # 溢出出队
-        if len(self.replay_buffer) > self.REPLAY_SIZE:
-            self.replay_buffer.popleft()
+        if (len(self.nstep_buffer) >= self.nsteps):
+            R = sum([self.nstep_buffer[i][2] * (self.GAMMA ** i) for i in range(self.nsteps)])
+            state, action, _, _ = self.nstep_buffer.pop(0)
+            if done:
+                self.nstep_buffer.clear()
+            # 存入replay_buffer
+            # self.replay_buffer = deque()
+            self.replay_buffer.append((state, one_hot_action, R, next_state, done))
 
+            # 溢出出队
+            if len(self.replay_buffer) > self.REPLAY_SIZE:
+                self.replay_buffer.popleft()
         # 可进行训练条件
         if len(self.replay_buffer) > self.BATCH_SIZE:
             self.train_Q_network(episode)
+
+    def get_transition(self):
+        """
+        Get transition index.
+        @mode random
+        :return:
+        """
+        indexes = []
+        for _ in range(self.BATCH_SIZE):
+            accepted = False
+            while not accepted:
+                point = np.random.randint(0, len(self.replay_buffer) - self.trace_length)
+                accepted = True
+                for i in range(self.trace_length-1):
+                    if self.replay_buffer[point+i][-1] > 0:
+                        accepted = False
+                        break
+                if accepted:
+                    for i in range(self.trace_length):
+                        indexes.append(point+i)
+
+        return np.array([self.replay_buffer[idx] for idx in indexes])
 
     def train_Q_network(self, episode):
         """
         Q网络训练
         :return:
         """
+        state_in = (np.zeros([self.BATCH_SIZE, self.hidden_size]), np.zeros([self.BATCH_SIZE, self.hidden_size]))
         self.time_step += 1
         # 从 replay buffer D中随机选取 batch size N条数据<s_j,a_j,r_j,s_j+1,done>$  D_selected
-        minibatch = random.sample(self.replay_buffer, self.BATCH_SIZE)
-        state_batch = [data[0] for data in minibatch]
-        action_batch = [data[1] for data in minibatch]
-        reward_batch = [data[2] for data in minibatch]
-        next_state_batch = [data[3] for data in minibatch]
+        minibatch = self.get_transition()
+        state_batch = minibatch[:,0].tolist()#[data[0] for data in minibatch]
+        action_batch = minibatch[:,1].tolist()#[data[1] for data in minibatch]
+        reward_batch = minibatch[:,2].tolist()#[data[2] for data in minibatch]
+        next_state_batch = minibatch[:,3].tolist()#[data[3] for data in minibatch]
 
         # 计算目标Q值y
         y_batch = []
-        QTarget_value_batch = self.target_net.Q_value.eval(feed_dict={self.target_net.state_input: next_state_batch})
-        Q_value_batch = self.main_net.Q_value.eval(feed_dict={self.main_net.state_input: next_state_batch})
-        for i in range(0, self.BATCH_SIZE):
+        QTarget_value_batch = self.target_net.Q_value.eval(feed_dict={self.target_net.state_input: next_state_batch,
+                                                                      self.target_net.batch_size: self.BATCH_SIZE,
+                                                                      self.target_net.train_length: self.trace_length
+                                                                      })
+        Q_value_batch = self.main_net.Q_value.eval(feed_dict={self.main_net.state_input: next_state_batch,
+                                                              self.main_net.batch_size: self.BATCH_SIZE,
+                                                              self.main_net.train_length: self.trace_length
+                                                              })
+        for i in range(0, len(minibatch)):
             done = minibatch[i][4]
             if done:
                 y_batch.append(reward_batch[i])
@@ -155,7 +208,10 @@ class Agent:
         self.optimizer.run(feed_dict={
             self.y_input: y_batch,
             self.action_input: action_batch,
-            self.main_net.state_input: state_batch
+            self.main_net.state_input: state_batch,
+            self.main_net.state_in: state_in,
+            self.main_net.batch_size: self.BATCH_SIZE,
+            self.main_net.train_length: self.trace_length
         })
 
         if episode % self.ckpt_interval and self.store_ckpt == 0:
@@ -169,8 +225,18 @@ class Agent:
         :return:
         """
         Q_value = self.main_net.Q_value.eval(feed_dict={
-            self.main_net.state_input: [state]
+            self.main_net.state_input: [state],
+            self.main_net.batch_size: 1,
+            self.main_net.train_length: 1
         })[0]
+
+        self.state_in = self.session.run(self.main_net.rnn_state, feed_dict={
+            self.main_net.state_input: [state],
+            self.main_net.state_in: self.state_in,
+            self.main_net.batch_size: 1,
+            self.main_net.train_length: 1
+        })
+
         if random.random() <= self.epsilon:
             self.epsilon -= (self.INITIAL_EPSILON - self.FINAL_EPSILON) / 10000
             return random.randint(0, self.action_dim - 1)
@@ -188,3 +254,6 @@ class Agent:
         if episode % self.REPLACE_TARGET_FREQ == 0:
             self.session.run(self.target_replace_op)
             # print('episode '+str(episode) +', target Q network params replaced!')
+
+    def reset_cell_state(self):
+        self.state_in = (np.zeros([1, self.hidden_size]), np.zeros([1, self.hidden_size]))
